d6340285835da16891276cbbf865aad487916832
league <league@wts14-4.attlocal.net>
2018-04-27 16:57:06 -0700

    drumkit duelingbuttons myfirstswinggui photoquiz

--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -1,8 +1,8 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 1
  */
-
 
 import java.io.IOException;
 
@@ -14,40 +14,40 @@ import javax.swing.JPanel;
 
 public class MyFirstSwingGUI {
 	public static void main(String[] args) {
-		//1. Create and initialize an object of the JFrame class
+		// 1. Create and initialize an object of the JFrame class
+		JFrame f1 = new JFrame();
+		// 2. Set your JFrame object to be visible
+		f1.setVisible(true);
+		// 3. Run your program. Do you see your window? It's probably very small.
 
-		//2. Set your JFrame object to be visible
+		// 4. Set the default close operation to JFrame.EXIT_ON_CLOSE
+		f1.setDefaultCloseOperation(f1.EXIT_ON_CLOSE);
+		// 5. Create and initialize an object of the JPanel class
+		JPanel p1 = new JPanel();
+		// 6. Create and initialize an object of the JLabel class
+		JLabel l1 = new JLabel();
+		// 7. Set the text of the JLabel to a lovely greeting.
+		l1.setText("Hello, you are wonderful.");
+		// 8. Add the JPanel object to the JFrame
+		f1.add(p1);
+		// 9. Add the JLabel object to the JPanel
+		p1.add(l1);
+		// 10. Pack your JFrame.
+		f1.pack();
+		// 11. Run your program again. Do you see your message.
 
-		//3. Run your program. Do you see your window? It's probably very small.
-		
-		//4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
-		//5. Create and initialize an object of the JPanel class
-
-		//6. Create and initialize an object of the JLabel class
-
-		//7. Set the text of the JLabel to a lovely greeting.
-
-		//8. Add the JPanel object to the JFrame
-
-		//9. Add the JLabel object to the JPanel
-
-		//10. Pack your JFrame.
-
-		//11. Run your program again. Do you see your message.
-		
-		//12. Use the loadImage method to set the icon of the JLabel object.  
-
-		//13. Re-pack the JFrame object.
-		
-		//14. Run the program one more time. Do you see the image?
+		// 12. Use the loadImage method to set the icon of the JLabel object.
+		l1.setIcon(loadImage());
+		// 13. Re-pack the JFrame object.
+		f1.pack();
+		// 14. Run the program one more time. Do you see the image?
 	}
-	
-	public static ImageIcon loadImage(){
+
+	public static ImageIcon loadImage() {
 		try {
 			return new ImageIcon(ImageIO.read(new MyFirstSwingGUI().getClass().getResourceAsStream("java.png")));
 		} catch (IOException e) {
-			
+
 			return null;
 		}
 	}
--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -1,8 +1,8 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 1
  */
-
 
 import java.awt.Component;
 import java.net.MalformedURLException;
@@ -19,36 +19,49 @@ public class PhotoQuiz {
 	public static void main(String[] args) throws Exception {
 		JFrame quizWindow = new JFrame();
 		quizWindow.setVisible(true);
-                quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
+		quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // This will make sure the program exits when you
+																	// close the window
 
-		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+		// 1. find an image on the internet, and put its URL in a String variable (from
+		// your browser, right click on the image, and select “Copy Image Address”)
+		String image = ("https://img.purch.com/h/1400/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OS85NjAvb3JpZ2luYWwvY29tbW9uLW9jdG9wdXMuanBn");
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component imageHolder;
 		// 3. use the "createImage()" method below to initialize your Component
-
+		imageHolder = createImage(image);
 		// 4. add the image to the quiz window
-
+		quizWindow.add(imageHolder);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String o = JOptionPane.showInputDialog("How many tentacles does an octopus have?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if (o.equals("8")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
-
-		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+		else {
+			JOptionPane.showMessageDialog(null, "INCORRECT");
+		}
+		// 9. remove the component from the quiz window (you may not see the effect of
+		// this until step 12)
+		quizWindow.remove(imageHolder);
 		// 10. find another image and create it (might take more than one line of code)
-
+		String image2 = ("https://img.aws.livestrongcdn.com/ls-article-image-673/ds-photo/getty/article/144/172/484499602.jpg");
+		Component imageHolder2;
+		imageHolder2 = createImage(image2);
 		// 11. add the second image to the quiz window
-
+		quizWindow.add(imageHolder2);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-
+		String d = JOptionPane.showInputDialog("What is the name of this fruit?");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+		if (d.equals("Dragonfruit")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+		} else {
+			JOptionPane.showMessageDialog(null, "INCORRECT");
+		}
 	}
 
 	private static Component createImage(String imageUrl) throws MalformedURLException {
@@ -60,10 +73,6 @@ public class PhotoQuiz {
 
 	/* OPTIONAL */
 	// *14. add scoring to your quiz
-	// *15. make something happen when mouse enters image (imageComponent.addMouseMotionListener()) 
+	// *15. make something happen when mouse enters image
+	// (imageComponent.addMouseMotionListener())
 }
-
-
-
-
-
